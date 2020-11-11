e61b104315beb36adc61bf6dbd51b632c71bbfc0
xdyi <409070169@qq.com>
2020-11-11 13:13:20 +0800

    add multistream test

--- examples/multistream_test.py
+++ examples/multistream_test.py
@@ -1,10 +1,16 @@
 from functools import partial
 from jax import lax
 from jax import jit, grad, pmap
+from jax import random, pmap
 import jax.numpy as jnp
 
 @partial(pmap, axis_name='i')
 def normalize(x):
   return x / lax.psum(x, 'i')
 
-print(normalize(jnp.arange(4.)))
+
+keys = random.split(random.PRNGKey(0), 8)
+mats = pmap(lambda key: random.normal(key, (5000, 6000)))(keys)
+
+
+print(mats)
